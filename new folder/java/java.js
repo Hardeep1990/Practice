9f8f95d9a794a813477ed6d441b45db8e5f57d5f
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2022-09-08 14:13:27 +1200

    another file of java

--- new folder/java/java.js
+++ new folder/java/java.js
@@ -1,1 +1,19 @@
-console.log("I am hardeep");
+let currentColorIndex = 0;
+//document.body.style.backgroundcolor=colorArray[currentColorIndex];
+// Function to be called on button being clicked
+function changeColor() {
+  //                      0       1     2       3         4         5        6         7
+  const colorArray = ['white', 'blue', 'red', 'green', 'yellow', 'orange', 'pink' ,'skyblue'];
+  if (currentColorIndex === 7) {
+    currentColorIndex = 0; // goes from index 5 to index 0, ie orange to white
+  } else {
+    currentColorIndex++;
+  }
+  document.body.style.backgroundColor = colorArray[currentColorIndex];
+}
+
+//const elementHeader = document.getElementById('elementHeader');
+const elementHeader = document.querySelector('#elementHeader');
+elementHeader.style.fontSize = '100px';
+elementHeader.style.fontFamily = 'Arial';
+elementHeader.textContent = ' I have changed text.';
