c40ae09715d54cf678ae53a4d4168a4d4ce90e1e
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2022-09-11 12:12:40 +1200

    new editing

---
+++ app.js
@@ -0,0 +1,16 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname)
+{
+    for(tablinks of tablinks)
+    {
+        tablinks.classList.remove("active-link");
+    }
+    for(tabcontents of tabcontents)
+    {
+tabcontents.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+    }
