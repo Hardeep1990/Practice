6ea1d1b990320a4089f90c6c617bc023b36597f4
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2022-09-14 13:25:00 +1200

    modification

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-function changeColor()
-   {
-document.body.style.backgroundColor = 'skyblue';
- }
-  const btnElement = document.getElementById('btn-skyblue');
-  btnElement.addEventListener('onclick', changeColor);
+
+
+function changeColor() {
+   console.log ("fuction called");
+   document.body.style.backgroundColor = 'skyblue';
+}
+function showTabContent(event, tabNo) {
+
+	const navTabElement = document.getElementsByClassName('tab-content')[0];
+	const divElements = navTabElement.querySelectorAll('div'); // ALL div with the content to show/hide
+
+	// Looping through the three divs & hiding ALL of them
+	divElements.forEach(divElement => {
+		divElement.style.display = 'none'; // hiding all
+	})
+
+	// Showing the selected div
+	const tabToShow = document.getElementById(`tab-${tabNo}`);
+	tabToShow.style.display = 'initial'; //showing just one
+}
+
+showTabContent( {}, 1 );
