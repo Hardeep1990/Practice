b057cd1f204f42f2e53ed1b7fb9e85380cd438bb
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2022-09-16 14:30:24 +1200

    new files

---
+++ new folder/java/java11.js
@@ -0,0 +1,13 @@
+// Creating a new html div element
+const divElement = document.createElement('div');
+divElement.innerText = 'Hello World';
+
+
+// adding the div created to the body of the HTML doc
+
+document.body.appendChild(divElement);
+const imageElement = document.createElement('img');
+
+imageElement.src = '/mission2/new folder/asees.jpg';
+
+document.body.appendChild(imageElement);
