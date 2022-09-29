076ed379484d0191409a4e476fae9b02d5f2bd1b
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2022-09-29 14:22:05 +1300

    add new features

--- react-webpage/src/App.js
+++ react-webpage/src/App.js
@@ -5,6 +5,7 @@ function App() {
   const updateCount = () => setCount(count + 1);
   return (
     <>
+      <h1> We are working with branches</h1>
       <Counter count={count} updateCount={updateCount} color="orange"></Counter>
       <br />
       <Counter count={count} updateCount={updateCount} color="blue"></Counter>
