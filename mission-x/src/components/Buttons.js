71f5d61a99a0cc2c88367466421a87e260005ee5
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2022-10-04 17:29:27 +1300

    navigation bar trouble

--- mission-x/src/components/Buttons.js
+++ mission-x/src/components/Buttons.js
@@ -1,15 +1,14 @@
-
+import './Button.css';
 function Button() {
     return (
-        <div style={{ textAlign: "pink" }}>
-            <button>BEGINNER</button>
-            <button>INTERMEDIATE</button>
-            <button>ADVANCED</button>
-            SHOW <button>25</button>
-            <button>50</button>
-            <button>100</button>
-
-        </div>
+        <>
+            <div className='btn_btn'> <button>BEGINNER</button>
+                <button>INTERMEDIATE</button>
+                <button>ADVANCED</button></div>
+            <div className="btn"> SHOW <button>25</button>
+                <button>50</button>
+                <button>100</button></div>
+        </>
     );
 }
 export default Button;
